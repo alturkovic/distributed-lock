7d7191633ef223a5f229c9f961edb80d3c8bba2f
Alen Turkovic <alturkovic@gmail.com>
2023-11-29 09:56:52 +0100

    fix(example): Fixed example

--- distributed-lock-example/src/main/resources/data.sql
+++ distributed-lock-example/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- do not rename this file, Spring Boot runs 'data.sql' on startup
 
-CREATE TABLE lock (
+CREATE TABLE distributed_lock (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     lock_key varchar(255) UNIQUE,
     token varchar(255),
@@ -10,6 +10,6 @@ CREATE TABLE lock (
 
 create trigger log_trigger
 after insert
-on lock
+on distributed_lock
 for each row
 call "com.github.alturkovic.lock.example.trigger.LoggingDatabaseTrigger"
